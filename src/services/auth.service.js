b59c6f1b14e9e0c4fe7946b2bec12082c82b1b5f
MiXProjecT <Vovan4ik112@gmail.com>
2020-08-12 16:18:21 +0500

    Update auth.service.js

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://pizza-delivery-back.herokuapp.com/api/auth';
+const API_URL = 'https://pizza-delivery-back.herokuapp.com/api/auth/';
 
 class AuthService {
   login(username, password) {
